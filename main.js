9a6bca4d0644ea87e62ee009b6590c4800fdbf52
Cix8 <francesco.natal_fn@libero.it>
2022-05-03 15:16:51 +0200

    add milestone1

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-const posts = [
-    {
-        "id": 1,
+const posts = [];
+
+const namesArray = ["Phil Mangione","Manuelita Benassai","Oldano Abela","Zelmina Arlotti","Ariele Straccioni"];
+console.log(namesArray.length);
+
+const objNumber = 5;
+for (let i = 0; i < objNumber; i++) {
+    const newObj = {
+        "id": i + 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "media": `https://unsplash.it/600/300?image=${getRndInteger(1,200)}`,
         "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+            "name": namesArray[i],
+            "image": "https://unsplash.it/300/300?image="+(i+1)
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        "likes": getRndInteger(1,200),
+        "created": `${getRndInteger(2000, 2022)} - ${getRndInteger(1,12)} - ${getRndInteger(1,28)}`
     }
-];
+
+    posts.push(newObj)
+}
+
+console.log(posts);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
